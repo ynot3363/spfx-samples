1936de545fb60d1fb0444384a93b2dbc643ef60a
Anthony Poulin <ynot3363@gmail.com>
2022-12-31 12:15:26 -0500

    Updated to use panel and support multiple pages

--- news/README.md
+++ news/README.md
@@ -2,47 +2,53 @@
 
 ## Summary
 
-Short summary on functionality and used technologies.
+Demonstrates how to add in additional commands to manage News Posts within a Site's Site Pages library. This extension will provide the abilitiy to Demote News Posts, Promote Pages to News Posts and update the Publish Date of News Posts.
 
-[picture of the solution in action, if possible]
+**Demote News Post**
+![screenshot of demote news post command]()
+
+**Promote / Update Publishing Date News Post**
+![screenshot of promote / update publishing date news post commands]()
+
+**Update Publishing Date Panel**
+![screenshot of the panel when updating publising date]()
 
 ## Used SharePoint Framework Version
 
-![version](https://img.shields.io/badge/version-1.13-green.svg)
+![version](https://img.shields.io/badge/version-1.14-green.svg)
 
 ## Applies to
 
 - [SharePoint Framework](https://aka.ms/spfx)
 - [Microsoft 365 tenant](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-developer-tenant)
 
-> Get your own free development tenant by subscribing to [Microsoft 365 developer program](http://aka.ms/o365devprogram)
-
 ## Prerequisites
 
-> Any special pre-requisites?
+The solution depends on having a Site Pages library created on the site and the Promoted State column added all the views.
 
 ## Solution
 
-Solution|Author(s)
---------|---------
-folder name | Author details (name, company, twitter alias with link)
+| Solution | Author(s)                                                                                                   |
+| -------- | ----------------------------------------------------------------------------------------------------------- |
+| news     | [Anthony Poulin](https://anthonyepoulin.com) follow me on Twitter [@ynot3363](https://twitter.com/ynot3363) |
 
 ## Version history
 
-Version|Date|Comments
--------|----|--------
-1.1|March 10, 2021|Update comment
-1.0|January 29, 2021|Initial release
+| Version | Date              | Comments        |
+| ------- | ----------------- | --------------- |
+| 1.0     | December 31, 2022 | Initial release |
 
 ## Disclaimer
 
-**THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
+**THIS CODE IS PROVIDED _AS IS_ WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
 
 ---
 
 ## Minimal Path to Awesome
 
 - Clone this repository
+- Update the properties in the following files to match your env.
+  - config > serve.json
 - Ensure that you are at the solution folder
 - in the command-line run:
   - **npm install**
@@ -52,22 +58,11 @@ Version|Date|Comments
 
 ## Features
 
-Description of the extension that expands upon high-level summary above.
+The **news** command set will add the ability for users to demote news posts, promote pages to news posts and update the publishing date of news posts (which control the order they appear in the News web part). A user can perform the actions on 1 or many pages. If more than 1 page is selected then all the pages either need to be a news post or a page for the commands to appear.
 
 This extension illustrates the following concepts:
 
-- topic 1
-- topic 2
-- topic 3
-
-> Notice that better pictures and documentation will increase the sample usage and the value you are providing for others. Thanks for your submissions advance.
-
-> Share your web part with others through Microsoft 365 Patterns and Practices program to get visibility and exposure. More details on the community, open-source projects and other activities from http://aka.ms/m365pnp.
-
-## References
-
-- [Getting started with SharePoint Framework](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-developer-tenant)
-- [Building for Microsoft teams](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/build-for-teams-overview)
-- [Use Microsoft Graph in your solution](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/web-parts/get-started/using-microsoft-graph-apis)
-- [Publish SharePoint Framework applications to the Marketplace](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/publish-to-marketplace-overview)
-- [Microsoft 365 Patterns and Practices](https://aka.ms/m365pnp) - Guidance, tooling, samples and open-source controls for your Microsoft 365 development
+- Demote News Posts to Pages
+- Promote Pages to News Posts
+- Update the Publishing Date for News Posts
+- Leverage React components within a CommandSet
--- news/src/extensions/news/NewsCommandSet.ts
+++ news/src/extensions/news/NewsCommandSet.ts
@@ -1,4 +1,3 @@
-import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 import { Log } from "@microsoft/sp-core-library";
 import {
   BaseListViewCommandSet,
@@ -8,11 +7,14 @@ import {
   RowAccessor,
 } from "@microsoft/sp-listview-extensibility";
 
-import * as strings from "NewsCommandSetStrings";
 import { override } from "@microsoft/decorators";
 import * as React from "react";
 import * as ReactDom from "react-dom";
-import { DialogWrapper, IDialogWrapperProps } from "./components/DialogWrapper";
+import {
+  CustomPanel,
+  ICustomPanelProps,
+} from "../../components/CustomPanel/CustomPanel";
+import { SPPermission } from "@microsoft/sp-page-context";
 
 /**
  * If your command set uses the ClientSideComponentProperties JSON input,
@@ -24,13 +26,32 @@ export interface INewsCommandSetProperties {}
 const LOG_SOURCE: string = "NewsCommandSet";
 
 export default class NewsCommandSet extends BaseListViewCommandSet<INewsCommandSetProperties> {
-  private dialogPlaceholder: HTMLDivElement = null;
+  private panelPlaceHolder: HTMLDivElement = null;
 
   @override
   public onInit(): Promise<void> {
     Log.info(LOG_SOURCE, "Initialized NewsCommandSet");
 
-    this.dialogPlaceholder = document.body.appendChild(
+    // Try to get the three commands
+    const compareOneCommand: Command = this.tryGetCommand("DEMOTE_NEWS_POST");
+    const compareTwoCommand: Command = this.tryGetCommand("PROMOTE_NEWS_POST");
+    const compareThreeCommand: Command = this.tryGetCommand(
+      "UPDATE_PUBLISHING_DATE"
+    );
+
+    // Ensure that the buttons are not visible by default
+    if (compareOneCommand) {
+      compareOneCommand.visible = false;
+    }
+    if (compareTwoCommand) {
+      compareTwoCommand.visible = false;
+    }
+    if (compareThreeCommand) {
+      compareThreeCommand.visible = false;
+    }
+
+    // Create the container for our React component
+    this.panelPlaceHolder = document.body.appendChild(
       document.createElement("div")
     );
 
@@ -46,342 +67,128 @@ export default class NewsCommandSet extends BaseListViewCommandSet<INewsCommandS
       "UPDATE_PUBLISHING_DATE"
     );
 
-    if (compareOneCommand) {
-      let isVisible = false;
+    // Check to see if the user has edit permissions to the list
+    const userHasEditPermissions: boolean =
+      this.context.pageContext.list.permissions.hasPermission(
+        SPPermission.editListItems
+      );
 
-      if (event.selectedRows.length === 1) {
-        isVisible =
-          event.selectedRows[0].getValueByName("PromotedState") === "2";
+    const allItemsPromoted = event.selectedRows.every(
+      (row) => row.getValueByName("PromotedState") === "2"
+    );
+    const allItemsNotPromoted = event.selectedRows.every(
+      (row) => row.getValueByName("PromotedState") === "0"
+    );
+
+    if (userHasEditPermissions) {
+      if (compareOneCommand) {
+        let isVisible = false;
+
+        if (allItemsPromoted) {
+          isVisible = true;
+        }
+
+        compareOneCommand.visible = isVisible;
       }
 
-      compareOneCommand.visible = isVisible;
-    }
+      if (compareTwoCommand) {
+        let isVisible = false;
 
-    if (compareTwoCommand) {
-      let isVisible = false;
+        if (allItemsNotPromoted) {
+          isVisible = true;
+        }
 
-      if (event.selectedRows.length === 1) {
-        isVisible =
-          event.selectedRows[0].getValueByName("PromotedState") === "0";
+        compareTwoCommand.visible = isVisible;
       }
 
-      compareTwoCommand.visible = isVisible;
-    }
+      if (compareThreeCommand) {
+        let isVisible = false;
 
-    if (compareThreeCommand) {
-      let isVisible = false;
+        if (allItemsPromoted) {
+          isVisible = true;
+        }
 
-      if (event.selectedRows.length === 1) {
-        isVisible =
-          event.selectedRows[0].getValueByName("PromotedState") === "2";
+        compareThreeCommand.visible = isVisible;
       }
-
-      compareThreeCommand.visible = isVisible;
     }
   }
 
   public onExecute(event: IListViewCommandSetExecuteEventParameters): void {
+    const items: RowAccessor[] = [...event.selectedRows];
+
     switch (event.itemId) {
       case "DEMOTE_NEWS_POST":
-        /**
-         * Loop through the selectedRows, there should only ever be 1 selected item though
-         * because the command is only visible when 1 item is select and it is promoted
-         */
-        event.selectedRows.forEach(async (item) => {
-          try {
-            const checkedOutUser = item.getValueByName("CheckoutUser");
-            const checkedOutUserId = item.getValueByName("CheckedOutUserId");
-            const currentUserId =
-              this.context.pageContext.legacyPageContext.userId.toString();
-            const promotedState = item.getValueByName("PromotedState");
-
-            if (promotedState === "2") {
-              /**
-               * Ensure that the page is not currently checked out to anyone before attempting to demote it.
-               */
-              if (checkedOutUserId && checkedOutUserId !== currentUserId) {
-                this.renderDialogWrapper(
-                  "Page is currently checked out",
-                  `The page is currently checked out to ${checkedOutUser[0].title}. You need to take ownership of the page before you can demote it.`,
-                  true
-                );
-                return;
-              }
-
-              const demoteNewsResponse = await this.demoteNewsPost(item);
-
-              if (demoteNewsResponse.ok) {
-                const data = await demoteNewsResponse.json();
-                let hasException = false;
-                let errorMessage = "";
-
-                /**
-                 * Loop through the fields in the response and ensure there are no errors
-                 * if there are set hasException to true and errorMessage
-                 */
-                data.value.forEach((field: any) => {
-                  if (field.HasException) {
-                    hasException = true;
-                    errorMessage = field.ErrorMessage;
-                  }
-                });
-
-                if (hasException) {
-                  this.renderDialogWrapper(
-                    "Error demoting page",
-                    `There was an error demoting the new post: ${errorMessage}`,
-                    true
-                  );
-                  return;
-                } else {
-                  const publishPageResponse = await this.publishPage(item);
-
-                  if (publishPageResponse.ok) {
-                    this.renderDialogWrapper(
-                      "Page demoted successfully",
-                      `The news post, ${item.getValueByName(
-                        "FileLeafRef"
-                      )}, has been successfully demoted.`,
-                      true
-                    );
-                    return;
-                  } else {
-                    this.renderDialogWrapper(
-                      "Error publishing page",
-                      `The news post, ${item.getValueByName(
-                        "FileLeafRef"
-                      )}, has been demoted but there was an error publishing the page. Please re-publish the page.`,
-                      true
-                    );
-                    return;
-                  }
-                }
-              } else {
-                this.renderDialogWrapper(
-                  "Error demoting page",
-                  `There was an error trying to demote the news post, ${demoteNewsResponse.statusText}.`,
-                  true
-                );
-                return;
-              }
-            }
-          } catch (error) {
-            Log.error(LOG_SOURCE, error);
-            this.renderDialogWrapper("Error", `${error}`, true);
-            return;
-          }
-        });
-
+        try {
+          this._showPanel(
+            items,
+            "Demote News Posts",
+            "Update the pages below to no longer be classified as News posts.",
+            "DEMOTE_NEWS_POST"
+          );
+        } catch (error) {
+          Log.error(LOG_SOURCE, error);
+        }
         break;
       case "PROMOTE_NEWS_POST":
-        /**
-         * Loop through the selectedRows, there should only ever be 1 selected item though
-         * because the command is only visible when 1 item is select and it is promoted
-         */
-        event.selectedRows.forEach(async (item) => {
-          try {
-            const checkedOutUser = item.getValueByName("CheckoutUser");
-            const checkedOutUserId = item.getValueByName("CheckedOutUserId");
-            const currentUserId =
-              this.context.pageContext.legacyPageContext.userId.toString();
-            const promotedState = item.getValueByName("PromotedState");
-
-            if (promotedState === "0") {
-              /**
-               * Ensure that the page is not currently checked out to anyone before attempting to promoting it.
-               */
-              if (checkedOutUserId && checkedOutUserId !== currentUserId) {
-                this.renderDialogWrapper(
-                  "Page is currently checked out",
-                  `The page is currently checked out to ${checkedOutUser[0].title}. You need to take ownership of the page before you can promote it.`,
-                  true
-                );
-                return;
-              }
-
-              const promoteNewsResponse = await this.promoteNewsPost(item);
-
-              if (promoteNewsResponse.ok) {
-                const data = await promoteNewsResponse.json();
-                let hasException = false;
-                let errorMessage = "";
-
-                /**
-                 * Loop through the fields in the response and ensure there are no errors
-                 * if there are set hasException to true and errorMessage
-                 */
-                data.value.forEach((field: any) => {
-                  if (field.HasException) {
-                    hasException = true;
-                    errorMessage = field.ErrorMessage;
-                  }
-                });
-
-                if (hasException) {
-                  this.renderDialogWrapper(
-                    "Error promoting page",
-                    `There was an error promoting the page to anew post: ${errorMessage}`,
-                    true
-                  );
-                  return;
-                } else {
-                  const publishPageResponse = await this.publishPage(item);
-
-                  if (publishPageResponse.ok) {
-                    this.renderDialogWrapper(
-                      "Page promoted successfully",
-                      `The page post, ${item.getValueByName(
-                        "FileLeafRef"
-                      )}, has been successfully promoted to a news post.`,
-                      true
-                    );
-                    return;
-                  } else {
-                    this.renderDialogWrapper(
-                      "Error publishing page",
-                      `The page, ${item.getValueByName(
-                        "FileLeafRef"
-                      )}, has been promoted to a news post but there was an error publishing the page. Please re-publish the page.`,
-                      true
-                    );
-                    return;
-                  }
-                }
-              } else {
-                this.renderDialogWrapper(
-                  "Error promoting page",
-                  `There was an error trying to demote the news post, ${promoteNewsResponse.statusText}.`,
-                  true
-                );
-                return;
-              }
-            }
-          } catch (error) {
-            Log.error(LOG_SOURCE, error);
-            this.renderDialogWrapper("Error", `${error}`, true);
-            return;
-          }
-        });
-
+        try {
+          this._showPanel(
+            items,
+            "Promote to News Posts",
+            "Update the pages below to be classified as News posts.",
+            "PROMOTE_NEWS_POST"
+          );
+        } catch (error) {
+          Log.error(LOG_SOURCE, error);
+        }
         break;
       case "UPDATE_PUBLISHING_DATE":
+        try {
+          this._showPanel(
+            items,
+            "Update Publishing Date",
+            "Update the publishing date for the selected pages below.",
+            "UPDATE_PUBLISHING_DATE"
+          );
+        } catch (error) {
+          Log.error(LOG_SOURCE, error);
+        }
         break;
       default:
         throw new Error("Unknown command");
     }
   }
 
-  /**
-   * Method will update the page and set the promoted state to 0
-   * @param item The item in the row that is currently being looped through
-   * @returns response from the ValidateUpdateListItem() call
-   */
-  private async demoteNewsPost(
-    item: RowAccessor
-  ): Promise<SPHttpClientResponse> {
-    const url = `${
-      this.context.pageContext.site.absoluteUrl
-    }/_api/web/lists(guid'${
-      this.context.pageContext.list.id
-    }')/items(${item.getValueByName("ID")})/ValidateUpdateListItem()`;
-    const body = {
-      formValues: [
-        {
-          FieldName: "PromotedState",
-          FieldValue: "0",
-        },
-      ],
-      bNewDocumentUpdate: false,
-    };
-
-    return await this.context.spHttpClient.post(
-      url,
-      SPHttpClient.configurations.v1,
-      {
-        headers: {
-          "Content-Type": "application/json;odata=nometadata",
-          "Content-Length": JSON.stringify(body).length.toString(),
-        },
-        body: JSON.stringify(body),
-      }
-    );
+  private _showPanel(
+    items: RowAccessor[],
+    title: string,
+    descripton: string,
+    command: string
+  ) {
+    this._renderPanelComponent({
+      context: this.context,
+      items: items,
+      isOpen: true,
+      panelTitle: title,
+      panelDescription: descripton,
+      command: command,
+      onRefresh: () => location.reload(),
+      onClose: () => this._renderPanelComponent({ isOpen: false }),
+    });
   }
 
-  /**
-   * Method will update the page and set the promoted state to 2
-   * @param item The item in the row that is currently being looped through
-   * @returns response from the ValidateUpdateListItem() call
-   */
-  private async promoteNewsPost(
-    item: RowAccessor
-  ): Promise<SPHttpClientResponse> {
-    const url = `${
-      this.context.pageContext.site.absoluteUrl
-    }/_api/web/lists(guid'${
-      this.context.pageContext.list.id
-    }')/items(${item.getValueByName("ID")})/ValidateUpdateListItem()`;
-    const body = {
-      formValues: [
-        {
-          FieldName: "PromotedState",
-          FieldValue: "2",
-        },
-      ],
-      bNewDocumentUpdate: false,
-    };
-
-    return await this.context.spHttpClient.post(
-      url,
-      SPHttpClient.configurations.v1,
+  private _renderPanelComponent(props: any) {
+    const element: React.ReactElement<ICustomPanelProps> = React.createElement(
+      CustomPanel,
       {
-        headers: {
-          "Content-Type": "application/json;odata=nometadata",
-          "Content-Length": JSON.stringify(body).length.toString(),
-        },
-        body: JSON.stringify(body),
+        isOpen: false,
+        items: [],
+        panelTitle: null,
+        panelDescription: null,
+        onClose: null,
+        onRefresh: null,
+        ...props,
       }
     );
-  }
-
-  /**
-   * Method will publish a page based on the server relative url of the item
-   * @param item The item in the row that is currently being looped through
-   * @returns response from the Publish() call
-   */
-  private async publishPage(item: RowAccessor): Promise<SPHttpClientResponse> {
-    const url = `${
-      this.context.pageContext.site.absoluteUrl
-    }/_api/web/GetFileByServerRelativeUrl('${item.getValueByName(
-      "FileRef"
-    )}')/Publish()`;
-
-    const body = { comment: "Demoted News Post" };
-
-    return await this.context.spHttpClient.post(
-      url,
-      SPHttpClient.configurations.v1,
-      {
-        headers: {
-          "Content-Type": "application/json;odata=nometadata",
-        },
-        body: JSON.stringify(body),
-      }
-    );
-  }
-
-  private closeErrorDialog = () => {
-    this.renderDialogWrapper("", "", false);
-  };
-
-  private renderDialogWrapper(title: string, subText: string, show: boolean) {
-    const element: React.ReactElement<IDialogWrapperProps> =
-      React.createElement(DialogWrapper, {
-        title: title,
-        subText: subText,
-        showDialog: show,
-        closeDialog: this.closeErrorDialog,
-      });
-
-    ReactDom.render(element, this.dialogPlaceholder);
+    ReactDom.render(element, this.panelPlaceHolder);
   }
 }
