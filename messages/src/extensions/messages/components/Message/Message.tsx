187bef57eef67072da529288cf198cdb8f23f424
Anthony Poulin <ynot3363@gmail.com>
2023-01-07 09:33:22 -0500

    Development (#5)
    * upgraded to 1.15.2
    
    * Upgraded footer and messages to 1.15.2

--- messages/src/extensions/messages/components/Message/Message.tsx
+++ messages/src/extensions/messages/components/Message/Message.tsx
@@ -1,9 +1,4 @@
-import {
-  IconButton,
-  IIconProps,
-  MessageBar,
-  MessageBarType,
-} from "@fluentui/react";
+import { IIconProps, MessageBar, MessageBarType } from "@fluentui/react";
 import * as React from "react";
 import { IMessage } from "../../../../models/IMessage";
 
@@ -33,10 +28,16 @@ const Message = ({ messages }: IMessageProps) => {
   return (
     <div id="messageContainer">
       {_messages.map((message: IMessage) => {
+        const linkUrl = new URL(message.link.url);
+        const isSharePointLink = linkUrl.hostname.indexOf("sharepoint") !== -1;
+
         return (
           <MessageBar
+            key={message.id}
             delayedRender={false}
-            messageBarType={MessageBarType[message.type]}
+            messageBarType={
+              MessageBarType[message.type as keyof typeof MessageBarType]
+            }
             isMultiline={true}
             dismissButtonAriaLabel="Close"
             dismissIconProps={dismissIconProps}
@@ -47,7 +48,12 @@ const Message = ({ messages }: IMessageProps) => {
           >
             <strong>{message.message} </strong>
             <span>{message.details}</span>
-            <a href={message.link.url} target="_blank">
+            <a
+              href={message.link.url}
+              target={isSharePointLink ? "_self" : "_blank"}
+              rel="noreferrer"
+              data-interception={isSharePointLink ? "on" : "off"}
+            >
               {message.link.desc}
             </a>
           </MessageBar>
