361f6446a7a682b631b1b7d0983a35f8793c7429
Anthony Poulin <ynot3363@gmail.com>
2024-12-19 11:25:31 -0500

    Upgraded to 1.20.0 and fixed loading issues

--- footer/src/extensions/footer/FooterApplicationCustomizer.ts
+++ footer/src/extensions/footer/FooterApplicationCustomizer.ts
@@ -2,7 +2,11 @@ import { ISPItem } from "./../../models/ISPItem";
 import * as React from "react";
 import * as ReactDom from "react-dom";
 import { Log } from "@microsoft/sp-core-library";
-import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
+import {
+  BaseApplicationCustomizer,
+  PlaceholderContent,
+  PlaceholderName,
+} from "@microsoft/sp-application-base";
 import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 
 import * as strings from "FooterApplicationCustomizerStrings";
@@ -30,6 +34,7 @@ export default class FooterApplicationCustomizer extends BaseApplicationCustomiz
    * Holds a collection of footer links from the associated list
    */
   private _footerItems: IFooterLink[] = [];
+  private _bottomPlaceholder: PlaceholderContent | undefined;
 
   /**
    * Overrides the onInit function and searchs for the canvas location to insert the
@@ -39,21 +44,28 @@ export default class FooterApplicationCustomizer extends BaseApplicationCustomiz
   public async onInit(): Promise<void> {
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
 
-    /**
-     * Looks for a specific element on in the SharePoint page DOM to append our
-     * customFooter div that we will render our Footer into.
-     */
-    const container: Element = document
-      .querySelector("#spCommandBar")
-      .nextElementSibling.querySelector(
-        `div[data-automation-id="contentScrollRegion"]`
-      ).children[0];
-
-    if (container) {
-      this._footerItems = await this.getItems();
+    if (!this.properties.intranetUrl || !this.properties.footerListId) {
+      const error: Error = new Error(
+        "Missing required configuration properties."
+      );
+      Log.error(LOG_SOURCE, error);
     }
 
-    this.context.application.navigatedEvent.add(this, this.renderFooter);
+    /**
+     * Without loading the placeholder the extension fails.
+     */
+    if (!this._bottomPlaceholder) {
+      this._bottomPlaceholder =
+        this.context.placeholderProvider.tryCreateContent(
+          PlaceholderName.Bottom,
+          { onDispose: this._onDispose }
+        );
+    }
+
+    this._footerItems = await this._getItems();
+
+    this._renderFooter();
+    this.context.application.navigatedEvent.add(this, this._renderFooter);
 
     return Promise.resolve();
   }
@@ -61,7 +73,7 @@ export default class FooterApplicationCustomizer extends BaseApplicationCustomiz
   /**
    * Searches for the canvas location to insert the footer into the page.
    */
-  private renderFooter(): void {
+  private _renderFooter(): void {
     const footerElement: Element = document.querySelector("#customFooter");
     /**
      * You need to get the container on the page in the renderFooter method because on partial
@@ -76,6 +88,7 @@ export default class FooterApplicationCustomizer extends BaseApplicationCustomiz
 
     if (!footerElement && container && this._footerItems.length > 0) {
       const footer: HTMLElement = document.createElement("footer");
+      footer.id = "custom-footer";
       const element: React.ReactElement<IFooterProps> = React.createElement(
         Footer,
         {
@@ -88,11 +101,18 @@ export default class FooterApplicationCustomizer extends BaseApplicationCustomiz
     }
   }
 
+  private _onDispose(placeholderContent: PlaceholderContent): void {
+    const footer = document.getElementById("custom-footer");
+    if (footer) {
+      ReactDom.unmountComponentAtNode(footer);
+    }
+  }
+
   /**
    * Makes a REST call to the associated list that holds the footer links
    * @returns A collection of IFooterLink
    */
-  private async getItems(): Promise<IFooterLink[]> {
+  private async _getItems(): Promise<IFooterLink[]> {
     const url: string = `${this.properties.intranetUrl}/_api/web/lists(guid'${this.properties.footerListId}')/items?$orderBy=linkOrder`;
 
     return new Promise<IFooterLink[]>((resolve, reject) => {
