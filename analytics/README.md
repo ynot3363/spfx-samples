fce8ea319f18cfdfba7763948c8d278817b323a9
Anthony Poulin <ynot3363@gmail.com>
2025-08-23 12:18:04 -0400

    Updated analytics solution to be CSP compliant

--- analytics/README.md
+++ analytics/README.md
@@ -2,37 +2,35 @@
 
 ## Summary
 
-Short summary on functionality and used technologies.
-
-[picture of the solution in action, if possible]
+Demonstrates how to add Google Analytics and Microsoft Clarity Analytics to SharePoint.
 
 ## Used SharePoint Framework Version
 
-![version](https://img.shields.io/badge/version-1.20.0-green.svg)
+![version](https://img.shields.io/badge/version-1.20-green.svg)
 
 ## Applies to
 
 - [SharePoint Framework](https://aka.ms/spfx)
 - [Microsoft 365 tenant](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-developer-tenant)
 
-> Get your own free development tenant by subscribing to [Microsoft 365 developer program](http://aka.ms/o365devprogram)
-
 ## Prerequisites
 
-> Any special pre-requisites?
+The solution depends on you having setup Google Analytics and Microsoft Clarity projects and having a tracking code for each.
 
 ## Solution
 
-| Solution    | Author(s)                                               |
-| ----------- | ------------------------------------------------------- |
-| folder name | Author details (name, company, twitter alias with link) |
+| Solution | Author(s)                                                                                                   |
+| -------- | ----------------------------------------------------------------------------------------------------------- |
+| footer   | [Anthony Poulin](https://anthonyepoulin.com) follow me on Twitter [@ynot3363](https://twitter.com/ynot3363) |
 
 ## Version history
 
-| Version | Date             | Comments        |
-| ------- | ---------------- | --------------- |
-| 1.1     | March 10, 2021   | Update comment  |
-| 1.0     | January 29, 2021 | Initial release |
+## Version history
+
+| Version | Date             | Comments                           |
+| ------- | ---------------- | ---------------------------------- |
+| 1.0.1   | March 10, 2021   | Updated code that is CSP compliant |
+| 1.0.0   | January 29, 2021 | Initial release                    |
 
 ## Disclaimer
 
@@ -46,28 +44,14 @@ Short summary on functionality and used technologies.
 - Ensure that you are at the solution folder
 - in the command-line run:
   - **npm install**
-  - **gulp serve**
-
-> Include any additional steps as needed.
+- Update the properties in the following files to match your env.
+  - config > serve.json
+  - **gulp serve --config=analytics**
 
 ## Features
 
-Description of the extension that expands upon high-level summary above.
+The **analytics** extension will Google Analytics and Microsoft Clarity tracking to SharePoint pages.
 
 This extension illustrates the following concepts:
 
-- topic 1
-- topic 2
-- topic 3
-
-> Notice that better pictures and documentation will increase the sample usage and the value you are providing for others. Thanks for your submissions advance.
-
-> Share your web part with others through Microsoft 365 Patterns and Practices program to get visibility and exposure. More details on the community, open-source projects and other activities from http://aka.ms/m365pnp.
-
-## References
-
-- [Getting started with SharePoint Framework](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-developer-tenant)
-- [Building for Microsoft teams](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/build-for-teams-overview)
-- [Use Microsoft Graph in your solution](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/web-parts/get-started/using-microsoft-graph-apis)
-- [Publish SharePoint Framework applications to the Marketplace](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/publish-to-marketplace-overview)
-- [Microsoft 365 Patterns and Practices](https://aka.ms/m365pnp) - Guidance, tooling, samples and open-source controls for your Microsoft 365 development
+- Adding analytics tracking to SharePoint
--- analytics/src/extensions/analytics/AnalyticsApplicationCustomizer.ts
+++ analytics/src/extensions/analytics/AnalyticsApplicationCustomizer.ts
@@ -1,17 +1,18 @@
-import { Log } from "@microsoft/sp-core-library";
 import { BaseApplicationCustomizer } from "@microsoft/sp-application-base";
+import { Log } from "@microsoft/sp-core-library";
+import * as strings from "AnalyticsApplicationCustomizerStrings";
 
 declare global {
   interface Window {
-    clarity: (action: string) => void;
-
+    clarity: {
+      (...args: unknown[]): void;
+      q?: unknown[][];
+    };
     gtag: (...args: unknown[]) => void;
-
     dataLayer?: unknown[];
+    [key: string]: boolean;
   }
 }
-
-import * as strings from "AnalyticsApplicationCustomizerStrings";
 
 const LOG_SOURCE: string = "AnalyticsApplicationCustomizer";
 
@@ -24,86 +25,111 @@ export interface IAnalyticsApplicationCustomizerProperties {}
 
 /** A Custom Action which can be run during execution of a Client Side Application */
 export default class AnalyticsApplicationCustomizer extends BaseApplicationCustomizer<IAnalyticsApplicationCustomizerProperties> {
-  private clarityId: string = "{YOUR_CLARITY_ID}"; // Replace with your Clarity ID
-  private clarityScriptId: string = "clarity-tracking-script";
-  private gaID: string = "{YOUR_GA_ID}"; // Replace with your Google Analytics ID
-  private gaScriptId: string = "ga-tracking-script";
+  private _clarityTrackingId: string = "[YOUR CLARITY CODE HERE]"; // Replace with your Clarity ID
+  private _clarityScriptId: string = "clarity-tracking-script";
+  private _gaTrackingID: string = "[YOUR G-CODE HERE]"; // Replace with your Google Analytics ID
+  private _gaScriptId: string = "ga-tracking-script";
+  private _calledOnce: boolean = false;
 
   public async onInit(): Promise<void> {
+    await super.onInit();
     Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
-    // Listen for navigation changes
-    this.context.placeholderProvider.changedEvent.add(
-      this,
-      this.applyTrackingCode
-    );
-    /* This event is triggered when user navigate between the pages */
-    this.context.application.navigatedEvent.add(this, this.applyTrackingCode);
+
+    if (!this._calledOnce) {
+      // Listen for placeholder placement changes
+      this.context.placeholderProvider.changedEvent.add(
+        this,
+        this.applyTrackingCode
+      );
+      /* This event is triggered when user navigate between the pages */
+      this.context.application.navigatedEvent.add(this, this.applyTrackingCode);
+      this._calledOnce = true;
+    }
     return Promise.resolve();
   }
 
   private async applyTrackingCode(): Promise<void> {
-    this.loadClarity();
-    this.loadGA();
+    const someTestCondition = true; // Replace with your actual condition
+
+    if (someTestCondition) {
+      this._addClarityAnalytics();
+      this._addGoogleAnalytics();
+    } else {
+      this._removeClarityAnalytics();
+      this._removeGoogleAnalytics();
+    }
   }
 
-  private loadGA(): void {
-    if (!document.getElementById(this.gaScriptId)) {
-      const script = document.createElement("script");
-      script.id = this.gaScriptId;
-      script.async = true;
-      script.src = `https://www.googletagmanager.com/gtag/js?id=G-${this.gaID}`;
-      script.onload = () => {
-        // Safe to initialize here
-        window.dataLayer = window.dataLayer || [];
-        function gtag(...args: unknown[]): void {
-          window.dataLayer?.push(args);
-        }
-        gtag("js", new Date());
-        gtag("config", `G-${this.gaID}`, {
-          siteName: this.context.pageContext.web.title,
-        });
+  private _addGoogleAnalytics(): void {
+    if (!document.getElementById(this._gaScriptId)) {
+      //First we need to define the dataLayer and gtag function globally
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function (): void {
+        // eslint-disable-next-line prefer-rest-params
+        window.dataLayer!.push(arguments); // If you change this from arguments to ...args, it breaks
       };
 
-      // The commented-out code below is an alternative way to load Google Analytics, however
-      // because it creates the script element dynamically and passes the function as a string
-      // to the text property it will trigger a CSP violation in SPO now.
-      /**
+      //Create and load GA script
       const gaScript = document.createElement("script");
-      gaScript.id = this.gaScriptId;
+      gaScript.id = this._gaScriptId;
       gaScript.type = "text/javascript";
       gaScript.async = true;
-      gaScript.src = `https://www.googletagmanager.com/gtag/js?id=G-${this.gaID}`;
+      gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${this._gaTrackingID}`;
 
-      const gaScript2 = document.createElement("script");
-      gaScript2.id = this.gaScriptId2;
-      gaScript2.type = "text/javascript";
-      gaScript2.text = `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date()); gtag('config', 'G-${
-        this.gaID
-      }',{'siteName': '${JSON.stringify(
-        this.context.pageContext.web.title
-      )}'});`;
+      gaScript.onload = () => {
+        window.gtag("js", new Date());
+        window.gtag("config", `${this._gaTrackingID}`);
+        Log.info(LOG_SOURCE, "Google Analytics initialized");
+      };
 
       document.head.appendChild(gaScript);
-      document.head.appendChild(gaScript2);
-      */
-
-      document.head.appendChild(script);
       Log.info(LOG_SOURCE, "Google Analytics script loaded");
     } else {
       Log.info(LOG_SOURCE, "Google Analytics script already loaded");
     }
   }
 
-  private loadClarity(): void {
-    if (!document.getElementById(this.clarityScriptId)) {
-      const script = document.createElement("script");
-      script.id = this.clarityScriptId;
-      script.async = true;
-      script.src = `https://www.clarity.ms/tag/${this.clarityId}`;
-      document.head.appendChild(script);
+  private _removeGoogleAnalytics(): void {
+    const gaScript = document.getElementById(this._gaScriptId);
+    const gaDisableID = `ga-disable-${this._gaTrackingID}`;
+    window[gaDisableID] = true; // Disable tracking
+    if (gaScript) {
+      gaScript.remove();
+      Log.info(LOG_SOURCE, "Google Analytics script removed");
+    } else {
+      Log.info(LOG_SOURCE, "Google Analytics script not found");
+    }
+  }
+
+  private _addClarityAnalytics(): void {
+    if (!document.getElementById(this._clarityScriptId)) {
+      // First define the clarity function if it doesn't exist
+      window.clarity =
+        window.clarity ||
+        function (...args: unknown[]): void {
+          (window.clarity.q = window.clarity.q || []).push(args);
+        };
+
+      // Create and load Clarity script
+      const clarityScript = document.createElement("script");
+      clarityScript.id = this._clarityScriptId;
+      clarityScript.async = true;
+      clarityScript.src = `https://www.clarity.ms/tag/${this._clarityTrackingId}`;
+      document.head.appendChild(clarityScript);
       Log.info(LOG_SOURCE, "Clarity tracking script loaded");
     } else {
       Log.info(LOG_SOURCE, "Clarity tracking script already loaded");
+    }
+  }
+
+  private _removeClarityAnalytics(): void {
+    const clarityScript = document.getElementById(this._clarityScriptId);
+    if (clarityScript) {
+      window.clarity("stop");
+      clarityScript.remove();
+      Log.info(LOG_SOURCE, "Clarity tracking script removed");
+    } else {
+      Log.info(LOG_SOURCE, "Clarity tracking script not found");
     }
   }
 }
